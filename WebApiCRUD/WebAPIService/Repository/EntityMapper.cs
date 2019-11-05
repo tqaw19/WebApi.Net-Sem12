bb8d65bc9477048e213b20462d525c20ca7dbf6c
alejandro quesada tinco <tqaw19@gmail.com>
2019-11-04 19:39:47 -0500

    2nd commit

---
+++ WebApiCRUD/WebAPIService/Repository/EntityMapper.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIService.Repository
+{
+    public class EntityMapper<TSource, TDestination> where TSource : class where TDestination : class
+    {
+        public EntityMapper()
+        {
+            Mapper.CreateMap<Models.Product, Product>();
+            Mapper.CreateMap<Product, Models.Product>();
+        }
+        public TDestination Translate(TSource obj)
+        {
+            return Mapper.Map<TDestination>(obj);
+        }
+    }
+}
